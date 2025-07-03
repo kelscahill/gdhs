bb16263460de920aa359c2a69e8654a4de4c311f
Kelsey Cahill <kelscahill@gmail.com>
2025-07-03 19:43:58 -0400

    Plugin updates

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit89688acf207ec0d984adf485b3045e72::getLoader();
+return ComposerAutoloaderInit93bdb62bebef82c52195055be3f35689::getLoader();
